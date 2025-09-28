273a629193624abcfcc3bae83f62be89be452f10
Cowther Richmond <richmondcowther@gmail.com>
2025-09-28 21:40:16 +0000

    #Update

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div>
-        <h1 className="text-center mx-auto py-4 font-bold"> Welcome to Splash App </h1>
+        <h1> Welcome to Splash App </h1>
     </div>
   );
 }
